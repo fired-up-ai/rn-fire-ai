82e6270d43184d89e6b599d17cdbe7bb892ea144
sukrut sathaye <sukrutssathaye@gmail.com>
2024-07-07 10:55:50 +0200

    central export of all classes in web-firebase

---
+++ packages/web-firebase/index.ts
@@ -0,0 +1,19 @@
+import FirebaseCore from "./src/core/FirebaseCore";
+import AbstractAuth from "./src/models/AbstractAuth";
+import AuthService from "./src/services/AuthService";
+import DBService from "./src/services/DBService";
+import CloudStorageService from "./src/services/CloudStorageService";
+import AbstractFirestore from "./src/models/AbstractFirestore";
+import AbstractStorage from "./src/models/AbstractStorage";
+
+
+
+module.exports = {
+    FirebaseCore,
+    AbstractAuth,
+    AuthService,
+    DBService,
+    CloudStorageService,
+    AbstractFirestore,
+    AbstractStorage
+};
