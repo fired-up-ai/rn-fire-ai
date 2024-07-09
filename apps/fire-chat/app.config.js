dff4674cf46d6c3b763fac4c66d88c974216bee0
sukrut sathaye <sukrutssathaye@gmail.com>
2024-07-09 16:51:09 +0200

    add app config file

---
+++ apps/fire-chat/app.config.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+
+export default ({ config }) => {
+  return {
+    ...config,
+    android: {
+      ...config.android,
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+      
+    },
+    ios: {
+      ...config.ios,
+      googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
+    },
+  };
+};
