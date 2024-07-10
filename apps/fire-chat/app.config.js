ca7cc7623b74219481482790c5d826a2be960ecc
sukrut sathaye <sukrutssathaye@gmail.com>
2024-07-10 19:07:14 +0200

    add react-native-firebase as dependency

--- apps/fire-chat/app.config.js
+++ apps/fire-chat/app.config.js
@@ -12,5 +12,26 @@ export default ({ config }) => {
       ...config.ios,
       googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
     },
+    plugins: [
+      [
+        'expo-build-properties',
+        {
+          android: {
+            compileSdkVersion: 34,
+            targetSdkVersion: 34,
+            buildToolsVersion: '34.0.0',
+            kotlinVersion: '1.6.10'
+          },
+        },
+      ],
+      '@react-native-firebase/app',
+      '@react-native-firebase/auth',
+      '@react-native-firebase/firestore',
+      '@react-native-firebase/storage',
+      '@react-native-firebase/messaging',
+      '@react-native-firebase/functions',
+      '@react-native-firebase/analytics',
+      '@react-native-firebase/crashlytics',
+    ],
   };
 };
