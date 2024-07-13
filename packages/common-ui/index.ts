d266880f22c676db35c7d3f414941e23fdd4521e
sukrut sathaye <sukrutssathaye@gmail.com>
2024-07-13 21:30:33 +0200

    move index.ts to root of package

---
+++ packages/common-ui/index.ts
@@ -0,0 +1,15 @@
+// @fired-up-ai/common-ui/src/index.ts
+
+// Import all components
+import WelcomePage from './src/components/WelcomePage';
+import AuthButtons from './src/components/AuthButtons';
+import ValidatedText from './src/components/ValidatedText';
+import SignIn from './src/components/SignIn';
+
+// Export all components
+export {
+  WelcomePage,
+  AuthButtons,
+  ValidatedText,
+  SignIn,
+};
