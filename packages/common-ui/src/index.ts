41bc666ec7c9c5bd66d7308c6f4745a1ef1044e3
sukrut sathaye <sukrutssathaye@gmail.com>
2024-07-13 20:40:52 +0200

    add index for common-ui

---
+++ packages/common-ui/src/index.ts
@@ -0,0 +1,15 @@
+// @fired-up-ai/common-ui/src/index.ts
+
+// Import all components
+import WelcomePage from './components/WelcomePage';
+import AuthButtons from './components/AuthButtons';
+import ValidatedText from './components/ValidatedText';
+import SignIn from './components/SignIn';
+
+// Export all components
+export {
+  WelcomePage,
+  AuthButtons,
+  ValidatedText,
+  SignIn,
+};
