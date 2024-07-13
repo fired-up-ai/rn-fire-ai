8045436456ef8e20b4062a29f9bc4232be407f3f
sukrut sathaye <sukrutssathaye@gmail.com>
2024-07-13 13:52:05 +0200

    import dark theme and pass to paper-provider

--- apps/fire-chat/src/app/_layout.tsx
+++ apps/fire-chat/src/app/_layout.tsx
@@ -1,14 +1,24 @@
 import { Stack } from "expo-router";
 import { Slot } from 'expo-router';
 import { Provider as StoreProvider } from "react-redux";
-import { Provider as UIProvider } from "react-native-paper";
+import { Provider as UIProvider, MD3DarkTheme } from "react-native-paper";
 // import { SessionProvider } from '../ctx';
 import store from "../store/store";
+
+const theme = {
+  ...MD3DarkTheme,
+  colors: {
+    ...MD3DarkTheme.colors,
+    primary: "#ff0000",
+
+  },
+};
+
 
 export default function RootLayout() {
   return (
     <StoreProvider store={store}>
-      <UIProvider>
+      <UIProvider theme={theme}>
         <Stack>
           <Stack.Screen name="index" />
         </Stack>
