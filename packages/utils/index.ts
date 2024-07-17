a9b374f5f5d49e4aca92e61388854c8dad2cd290
sukrut29 <sukrutssathaye@gmail.com>
2024-07-17 13:23:29 +0200

    central import export of utilities

---
+++ packages/utils/index.ts
@@ -0,0 +1,9 @@
+import { AuthProvider, useAuth } from './src/ctx/AuthContext';
+import { useStorageState, setStorageItemAsync } from './src/hooks/useStorageState';
+
+export {
+    AuthProvider,
+    useAuth,
+    useStorageState,
+    setStorageItemAsync
+}
