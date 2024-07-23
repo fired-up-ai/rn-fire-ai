7a82860562c80b02ad39c78099d4b1cee0a771e2
sukrut29 <sukrutssathaye@gmail.com>
2024-07-23 14:41:42 +0200

    add root babel.config

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    presets: [
+      'module:metro-react-native-babel-preset',
+      '@babel/preset-react',
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      'react-native-paper/babel',
+    ],
+  };
+  
