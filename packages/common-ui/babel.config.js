3b2867e9062da9876f75babf621f960e31febaf1
sukrut29 <sukrutssathaye@gmail.com>
2024-07-23 18:25:39 +0200

    add babel config for paper

--- packages/common-ui/babel.config.js
+++ packages/common-ui/babel.config.js
@@ -3,7 +3,8 @@ module.exports = {
     plugins: [
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      'react-native-paper/babel',
     ]
   };
   
