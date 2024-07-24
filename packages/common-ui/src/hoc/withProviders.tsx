1d39f834c02de059bbf2a9ae67a2b89c899cd627
sukrut29 <sukrutssathaye@gmail.com>
2024-07-24 03:37:18 +0200

    fix import error caught by unit tests

--- packages/common-ui/src/hoc/withProviders.tsx
+++ packages/common-ui/src/hoc/withProviders.tsx
@@ -1,7 +1,7 @@
 // @fired-up-ai/common-ui/src/hoc/withProviders.tsx
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
-import { PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import {store} from '@fired-up-ai/store';
 
 const withProviders = (Component: any) => (props: any) => (
