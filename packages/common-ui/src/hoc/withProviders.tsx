38e2c7ec5e4a14d795487e12aa0d9209c9e291e5
sukrut29 <sukrutssathaye@gmail.com>
2024-07-24 15:38:22 +0200

    import Material design Light and Dark Themes in addition to Provider

--- packages/common-ui/src/hoc/withProviders.tsx
+++ packages/common-ui/src/hoc/withProviders.tsx
@@ -1,7 +1,11 @@
 // @fired-up-ai/common-ui/src/hoc/withProviders.tsx
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { 
+  Provider as PaperProvider,
+  MD3DarkTheme,
+  MD3LightTheme
+} from 'react-native-paper';
 import {store} from '@fired-up-ai/store';
 
 const withProviders = (Component: any) => (props: any) => (
